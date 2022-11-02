8a06ae8b085844a60c4ba06ed33fab30276b6569
Naresh <macbookpro@Nareshs-MBP.home-life.hub>
2022-11-02 10:53:33 +0100

    final commit

---
+++ app/contants/Index.js
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js";
+//import { idl } from "./idl.json";
+
+export const TODO_PROGRAM_PUBKEY = new PublicKey(
+  "9nSYrg6en7TqND2iLstK6t6hegfxKnyLudPmdaqq1ifj"
+);
+
+//export const IDL = JSON.parse("idl");
